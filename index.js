e3b8d3dd3fc4f3f8684b93ab1364c99fa914c5ec
Montse Garcia <montsegarciaj@hotmail.com>
2026-04-03 09:17:13 +0200

    actualizar pintado html

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 import APIManager from "./APIManager.js";
+import DOMManager from ".DOMManager.js";
+import Workshop from "./Workshop.js";
+import { WorkshopGeneric, WorkshopCollection } from "./Workshop.js";
 
 async function main() {
-  const rawPhotos = await APIManager.getData("yoga");
-  console.log(rawPhotos);
+  const workshopGross = await APIManager.getData();
+  const domManager = new DOMManager();
+  const container = document.querySelector(".product-container");
+  console.log(container);
+
+  const workshopGeneric = await workshopCreate(workshopGross.photos);
+
+  const workshopCollection = new WorkshopCollection();
+  workshopGeneric.getElements().forEach(workshop => {
+    const html = domManager.WorkshopCreateHtml(workshop);
+    container.appendChild(html);
+  });
 }
+
 main();
